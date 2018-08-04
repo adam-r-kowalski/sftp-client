7c06a86a51b199af6de5da70f44f3f8c25e0aef3
Adam Kowalski <adam.kowalski.work@gmail.com>
2018-08-03 17:10:20 -0700

    mock input is in place

--- client/src/input.rs
+++ client/src/input.rs
@@ -45,3 +45,55 @@ impl Input for ConsoleInput {
         rpassword::prompt_password_stdout("Enter password: ").unwrap()
     }
 }
+
+pub struct MockInput {
+    mock_string: String,
+    mock_prompt_path: PathBuf,
+    mock_path: PathBuf,
+}
+
+impl MockInput {
+    pub fn new() -> MockInput {
+        MockInput {
+            mock_string: String::new(),
+            mock_prompt_path: PathBuf::from(String::new()),
+            mock_path: PathBuf::from(String::new()),
+        }
+    }
+}
+
+impl MockInput {
+    pub fn set_string(&mut self, value: String) {
+        self.mock_string = value
+    }
+
+    pub fn set_prompt_path(&mut self, value: String) {
+        self.mock_prompt_path = PathBuf::from(value)
+    }
+
+    pub fn set_path(&mut self, value: String) {
+        self.mock_path = PathBuf::from(value)
+    }
+}
+
+impl Input for MockInput {
+    fn string(&self, _: &str) -> String {
+        self.mock_string.clone()
+    }
+
+    fn prompt_path(&self, _: &str) -> PathBuf {
+        self.mock_prompt_path.clone()
+    }
+
+    fn path(&self) -> PathBuf {
+        self.mock_path.clone()
+    }
+
+    fn positive(&self, _: &str) -> usize {
+        0
+    }
+
+    fn password(&self) -> String {
+        String::new()
+    }
+}

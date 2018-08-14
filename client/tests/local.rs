32f91e01279af3c1a342d46d623d6d094114bb88
Adam Kowalski <adam.kowalski.work@gmail.com>
2018-08-14 15:08:58 -0700

    fix local rename file test

--- client/tests/local.rs
+++ client/tests/local.rs
@@ -27,15 +27,16 @@ fn change_permissions() {
 #[test]
 fn rename_file() {
     let mut input = MockInput::new();
-    let path = Path::new("/demo_file21.txt");
-    let new_path = Path::new("/demo_file22.txt");
-    input.set_path(&path);
-    input.set_path(&new_path);
+    let source = Path::new("/demo_file21.txt");
+    let destination = Path::new("/demo_file22.txt");
+    input.set_paths(&[&source, &destination]);
+    input.set_prompt_paths(&[&source, &destination]);
 
     let mut connection = Connection::to_container(input);
 
-    File::create("/demo_file21.txt").unwrap();
+    local::create_file(&mut connection);
     local::rename_file(&mut connection);
-    assert_eq!(Path::new("/demo_file22.txt").exists(), false);
+    assert!(!local::file_exists(source));
+    assert!(local::file_exists(destination));
     local::delete_file(&mut connection);
 }
